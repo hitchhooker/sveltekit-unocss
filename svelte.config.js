ab64e740d1ec1bac6ba7e31db2c1855c2d02dc12
Tommi <tommi@niemi.lol>
2021-10-28 17:25:50 +0100

    imageprocessing add

--- svelte.config.js
+++ svelte.config.js
@@ -2,15 +2,25 @@
 import vercel from '@sveltejs/adapter-vercel';
 import Unocss from 'unocss/vite';
 import { presetUno, presetAttributify } from 'unocss';
-import presetIcons from '@unocss/preset-icons'
+import presetIcons from '@unocss/preset-icons';
+import imagePreprocessor from 'svimg';
 
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: vercel()
+		adapter: vercel(),
+		appDir: '_app'
 	},
+	preprocess: [
+        imagePreprocessor({
+            inputDir: 'static',
+            outputDir: 'static/optimized-images',
+            webp: true,
+            avif: false,
+        })
+	],
 	plugins: [
 		Unocss({
 			presets: [
